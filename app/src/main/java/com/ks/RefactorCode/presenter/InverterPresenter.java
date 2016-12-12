cde15e1da77affa17f6ec14ee126c5c27bf09fd5
liyu <liyu2140@gmail.com>
2016-12-12 18:22:54 +0800

    refirst3

--- app/src/main/java/com/ks/RefactorCode/presenter/InverterPresenter.java
+++ app/src/main/java/com/ks/RefactorCode/presenter/InverterPresenter.java
@@ -1,13 +1,13 @@
-package com.ks.RefactorCode.ui.adapter;
+package com.ks.RefactorCode.presenter;
 
 /**
  * @version V 1.0
  * @description:
  * @title: EzManage
- * @package com.ks.RefactorCode.ui.adapter
+ * @package com.ks.RefactorCode.presenter
  * @author: GoodWe
  * @date: 2016-12-12
  * @time: 18:19
  */
-public class BaseAdapter {
+public class InverterPresenter {
 }
