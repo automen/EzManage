7f56769c57a844fece708125223b8c40f29e357a
liyu <liyu2140@gmail.com>
2016-12-12 16:46:21 +0800

    Initial commit

---
+++ app/src/main/java/com/ks/RefactorCode/BaseApplication.java
@@ -0,0 +1,12 @@
+package com.ks.RefactorCode;
+
+import android.app.Application;
+
+/**
+ * Created by liyu
+ * on 2016/12/12.
+ */
+
+public class BaseApplication extends Application {
+
+}
---
+++ app/src/main/java/com/ks/RefactorCode/ui/adapter/BaseAdapter.java
@@ -0,0 +1,9 @@
+package com.ks.RefactorCode.ui.adapter;
+
+/**
+ * Created by liyu
+ * on 2016/12/12.
+ */
+
+public class BaseAdapter {
+}
